a11f50c60f01b3a7c55d4f8a0f34b3b71a47d5d9
harshkr-cs <harshwwe555@gmail.com>
2026-02-28 15:54:07 +0530

    StudentRegistrationSystemWithMVC

---
+++ SaturdayAssignment/28FebStudentRegistrationSystemWithMVC_ADONET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentRegistrationSystemWithMVC_ADONET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SaturdayAssignment/28FebStudentRegistrationSystemWithMVC_ADONET/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentRegistrationSystemWithMVC_ADONET.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string StudentCode { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string FatherName { get; set; }
+
+        [Required]
+        public string MotherName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[6-9]\d{9}$", ErrorMessage = "Invalid Mobile")]
+        public string Mobile { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public byte[] Photo { get; set; }
+    }
+}
